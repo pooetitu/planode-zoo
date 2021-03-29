5f1fc494f3b5d20e04343ad134116f0042d3885e
Isaac <siphron0@gmail.com>
2021-03-29 21:24:48 +0200

    [ADD] Espace Model

---
+++ src/models/espace.model.ts
@@ -0,0 +1,73 @@
+import {
+    BelongsToGetAssociationMixin,
+    BelongsToSetAssociationMixin,
+    DataTypes,
+    HasManyAddAssociationMixin,
+    HasManyGetAssociationsMixin,
+    Model,
+    ModelCtor,
+    Optional,
+    Sequelize
+} from "sequelize";
+import {SessionInstance, SessionProps} from "./session.model";
+import {UserInstance} from "./user.model";
+
+export interface EspaceProps {
+    id: number;
+    name: string;
+    type: string;
+    description: string;
+    capacity: number;
+    duration: number;
+    openingstime: string;
+    disabledaccess: boolean;
+    maintenance: boolean;
+
+
+}
+export interface EspaceCreationProps extends Optional<EspaceProps, "id"> {
+}
+
+export interface EspaceInstance extends Model<EspaceProps, EspaceCreationProps>, EspaceProps {
+
+}
+
+export default function (sequelize: Sequelize): ModelCtor<EspaceInstance> {
+    return sequelize.define<EspaceInstance>("Espace", {
+        id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            unique: true
+        },
+        type: {
+            type: DataTypes.STRING
+        },
+        description: {
+            type: DataTypes.STRING
+        },
+        capacity: {
+            type: DataTypes.BIGINT
+        },
+        duration: {
+            type: DataTypes.INTEGER
+        },
+        openingstime: {
+            type: DataTypes.STRING
+        },
+        disabledaccess: {
+            type: DataTypes.BOOLEAN
+        },
+        maintenance: {
+            type: DataTypes.BOOLEAN
+        },
+    }, {
+        freezeTableName: true,
+        underscored: true,
+        paranoid: true,
+        timestamps: true
+    });
+}
