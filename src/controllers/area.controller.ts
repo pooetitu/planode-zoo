2b29c7222f5232dca1e742cd598ed4faf50eda9b
Isaac <siphron0@gmail.com>
2021-03-31 19:15:05 +0200

    ADD createArea
    ???

--- src/controllers/area.controller.ts
+++ src/controllers/area.controller.ts
@@ -1,13 +1,15 @@
 import {ModelCtor} from "sequelize";
-import {UserCreationProps, UserInstance} from "../models/user.model";
+import {UserInstance} from "../models/user.model";
 import {SessionInstance} from "../models/session.model";
 import {SequelizeManager} from "../models";
-import {compare, hash} from "bcrypt";
-import {AreaInstance} from "../models/area.model";
+import {AreaCreationProps, AreaInstance} from "../models/area.model";
 import {AreaAccessInstance} from "../models/area_access.model";
 
 export class AreaController {
 
+
+    Area: ModelCtor<AreaInstance>;
+    AreaAccess: ModelCtor<AreaAccessInstance>;
     User: ModelCtor<UserInstance>;
     Session: ModelCtor<SessionInstance>;
 
@@ -15,17 +17,23 @@ export class AreaController {
 
     public static async getInstance(): Promise<AreaController> {
         if(AreaController.instance === undefined) {
-            const {User, Session} = await SequelizeManager.getInstance();
-            AreaController.instance = new AreaController(User, Session);
+            const {User, Session, Area, AreaAccess} = await SequelizeManager.getInstance();
+            AreaController.instance = new AreaController(User, Session, Area, AreaAccess);
         }
         return AreaController.instance;
     }
 
-    private constructor(User: ModelCtor<UserInstance>, Session: ModelCtor<SessionInstance>) {
+    private constructor(User: ModelCtor<UserInstance>, Session: ModelCtor<SessionInstance>, Area: ModelCtor<AreaInstance>, AreaAccess: ModelCtor<AreaAccessInstance>) {
         this.User = User;
         this.Session = Session;
+        this.Area = Area;
+        this.AreaAccess = AreaAccess;
     }
 
-
+    public async createArea(props: AreaCreationProps): Promise<AreaInstance | null>{
+        return this.Area.create({
+            ...props
+        });
+    }
 
 }
